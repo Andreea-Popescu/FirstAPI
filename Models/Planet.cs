c5fdd972ac9b82905fe2b5fa4cdbda497a7568c7
Andreea Popescu <andreeaiuliapopescu@gmail.com>
2026-09-03 17:12:59 +0300

    Added Planet.cs - WIP

---
+++ Models/Planet.cs
@@ -0,0 +1,12 @@
+namespace FirstAPI.models
+{
+    public class Planet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public List<Character> Characters { get; set; } = new();
+
+    }
+}
