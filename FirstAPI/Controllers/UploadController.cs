2c84add667a43e8ad22586d41f78e2599a9e3200
Andreea Popescu <andreeaiuliapopescu@gmail.com>
2026-09-09 23:15:06 +0300

    Added IJSRuntime for favorite functionality + Small navbar tweaks

--- FirstAPI/Controllers/UploadController.cs
+++ FirstAPI/Controllers/UploadController.cs
@@ -50,4 +50,42 @@ public async Task <IActionResult> UploadCharacterImage ([FromForm] IFormFile fil
         var relativeUrl = $"/images/characters/{uniqueFileName}";
         return Ok(new { url = relativeUrl });
     }
+
+    // POST pt upload fisiere api/upload/aeon-image
+    [HttpPost("aeon-image")]
+    public async Task <IActionResult> UploadAeonImage ([FromForm] IFormFile file)
+        {
+            // verificam sa nu fie gol fisierul
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("Nu a fost selectat niciun fișier.");
+            }
+
+            // Setam extensiile si verificam extensia fisierului
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".webp" };
+            var extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            if (!allowedExtensions.Contains(extension))
+            {
+                return BadRequest("Tip de fișier nepermis! Sunt acceptate doar JPG, PNG și WEBP.");
+            }
+
+            var uniqueFileName = $"{Guid.NewGuid()}{extension}"; // dam un nume unic fisierului incarcat
+            var uploadsFolder = Path.Combine(_environment.WebRootPath ?? Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"), "images", "aeons");
+            // spunem sa salveze fisieru pe calea wwwroot/images/aeons
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder); // dc nu exista, sa l creeze
+            }
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName); // aici pune fisieru in folder
+
+            using (var stream = new FileStream(filePath, FileMode.Create)) // scrierea binara
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            // returnam calea relativa spre fisier
+            var relativeUrl = $"/images/aeons/{uniqueFileName}";
+            return Ok(new { url = relativeUrl });
+        }
 }
